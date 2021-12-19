87e03eb8498a2d1f472925f9206679ceefa88ecc
Mark Wong <markw83uk@gmail.com>
2021-12-19 18:19:38 +0000

    local workaround, updates to support thumbnails.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,126 @@
 
 // initialise variables for later
-var projects = {};
+var projects = {
+    "Cooks Needed" : [
+        {
+            "20 Light Ration" : {
+                "img" : "https://cdn.nwdb.info/db/images/live/v3/icons/items_hires/foodhealthrecoveryt1.png",
+                "items" : [
+                    { "Light Ration" : 20}
+                ]
+            }
+        },
+        {   
+            "5 Travel Ration" : {
+                "img" : "https://cdn.nwdb.info/db/images/live/v3/icons/items_hires/foodhealthrecoveryt2.png",
+                "items" : [
+                    {"Travel Ration" : 5}
+                 ]        
+            }
+        },
+        {
+            "15 Travel Ration" : {
+                "img" : "https://cdn.nwdb.info/db/images/live/v3/icons/items_hires/foodhealthrecoveryt2.png",
+                "items" : [
+                    { "Travel Ration" : 15}
+                ]
+            }
+        },            
+        {
+            "Condiment Supplies" :{
+                "img" : "https://cdn.nwdb.info/db/images/live/v3/icons/items_hires/faction_provisions.png",
+                "items" : [
+                    {"Salt" : 12},
+                    {"Peppercorn" : 12},
+                    {"Cooking Oil" : 12}
+                ]
+            }
+        },            
+        {
+            "Carb Supplies" :{
+                "img" : "https://cdn.nwdb.info/db/images/live/v3/icons/items_hires/faction_provisions.png",
+                "items" : [
+                    {"Potato" : 12},
+                    {"Pasta" : 12},
+                    {"Bread" : 12}
+                ]
+            }
+        }
+    ],
+    "Fisherfolk Needed" : [
+
+        {
+            "15 Fish Filet" : {
+                "img" : "https://cdn.nwdb.info/db/images/live/v3/icons/items_hires/fisht1.png",
+                "items" : [
+                    {"Fish Filet" : 15}
+                ]
+            }
+        },
+        {
+            "30 Fish Filet" : {
+                "img" : "https://cdn.nwdb.info/db/images/live/v3/icons/items_hires/fisht1.png",
+                "items" : [
+                    {"Fish Filet" : 30}
+                ]
+            }
+        },
+        {
+            "15 Firm Fish Filet" : {
+                "img" : "https://cdn.nwdb.info/db/images/live/v3/icons/items_hires/fisht1.png",
+                "items" : [
+                    {"Firm Fish Filet" : 15}
+                ]
+            }
+        },
+        {
+            "30 Firm Fish Filet" : {
+                "img" : "https://cdn.nwdb.info/db/images/live/v3/icons/items_hires/fisht1.png",
+                "items" : [
+                    {"Firm Fish Filet" : 30}
+                ]
+            }
+        },                                    
+    ],
+    "Alchemists Needed" : [
+        {
+            "1 Strong Mana Potion" : {
+                "img" : "https://cdn.nwdb.info/db/images/live/v3/icons/items_hires/potionmanat3.png",
+                "items" : [
+                    {"Strong Mana Potion" : 1}
+                ]
+            }
+        }
+    ],
+    "Lumberjacks Needed" : [
+        {
+            "150 Green Wood" : {
+                "img" : "https://cdn.nwdb.info/db/images/live/v3/icons/items_hires/woodt1.png",
+                "items" : [
+                    {"Green Wood" : 150}
+                ]
+            }
+        },
+        {
+            "550 Green Wood" : {
+                "img" : "https://cdn.nwdb.info/db/images/live/v3/icons/items_hires/woodt1.png",
+                "items" : [
+                    {"Green Wood" : 550}
+                ]
+            }
+        }            
+    ],
+    "Stonemasons Needed" : [
+        {
+            "30 Stone Block" : {
+                "img" : "https://cdn.nwdb.info/db/images/live/v3/icons/items_hires/blockt2.png",
+                "items" : [
+                    {"Stone Block" : 30}
+                ]
+            }
+        }         
+    ]
+};
 var shopping_list = [];
 var basket = [];  
 
@@ -9,12 +129,14 @@ var basket = [];
 $(document).ready(function(){
 
     // load project json file into variable projects
+    /*
     $.getJSON("projects.json", function(data){
         projects = data;
         console.log(projects);
         drawProjectQuests(projects);
     });     
-
+    */
+    drawProjectQuests(projects);
 
     // on click of main project board category
     $(document).on('click','#projects .projectBoard',function(e){
@@ -54,7 +176,7 @@ function drawProjectQuests(projects){
 
     $.each(projects, function(index,jsonObject){
 
-        $('#projects').append('<div id="' + index + '" class="projectBoard tier1" data-type="' + index + '"><div class="projectBoardInner"><h3>' + index + '<h3></div></div>');
+        $('#projects').append('<div id="' + index + '" class="projectBoard tier1" data-type="' + index + '"><div class="projectBoardInner"><h3>' + index + '</h3></div></div>');
 
     });
 
@@ -72,7 +194,7 @@ function drawProjectSubQuests(projects,type){
 
             console.log(jsonObject);
 
-            $('#subProjects').append('<div id="' + key + '" class="projectBoard" data-index="' + index + '" data-type="' + type + '" data-subtype="' + key + '"><div class="projectBoardInner"><h3>' + key + '</h3></div></div>');
+            $('#subProjects').append('<div id="' + key + '" class="projectBoard" data-index="' + index + '" data-type="' + type + '" data-subtype="' + key + '"><div class="projectBoardInner"><img src="' + val.img + '" class="projectBoardImg"><h3>' + key + '</h3></div></div>');
 
         });
 
@@ -113,9 +235,13 @@ function drawShoppingList(){
 
             //console.log(val);
 
-            $.each(val, function(ikey,ival){
+            $.each(val['items'], function(ikey,ival){
+
+                console.log(ival);
 
                 $.each(ival, function(bkey,bval){
+
+                    console.log(bkey + ' ' + bval);
 
                     var itemClass = bkey.replaceAll(" ","");
 
@@ -125,7 +251,7 @@ function drawShoppingList(){
                         $('.basketItem.' +itemClass+' .basketQty').html(currentVal);
                         //console.log(currentVal);
                     }else{
-                        $('.basketItems').append('<li class="basketItem ' + itemClass  + '" data-sort="' + itemClass + '"><h3><span class="basketName">' + bkey + '</span> x <span class="basketQty">' + bval + '</span></h3></li>');
+                        $('.basketItems').append('<li class="basketItem ' + itemClass  + '" data-sort="' + itemClass + '"><h3><span class="basketQty">' + bval + '</span> <span class="basketName">' + bkey + '</span></h3></li>');
                     }
 
                 });
